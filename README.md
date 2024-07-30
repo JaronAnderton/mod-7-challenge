82432fe40e009013f9be20575141b23f40e6643f
Jaron Anderton <jaronanderton@macbook-air.mynetworksettings.com>
2024-07-30 00:00:32 -0600

    update generate markdown and index.js to fix formatting errors after several failed attempts. this is a delayed commit that should've been done several times as I was updating code trying to fix the problems.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# mod-7-challenge
+# my project
+  [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+  ## Description
+  description
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [License](#license)
+  
+  ## Installation
+  asdf
+  
+  ## Usage
+  asdf
+  
+  ## Contributing
+  asdf
+  
+  ## Tests
+  asdf
+  
+  ## Questions
+  For any questions, please contact me at [asdf](mailto:asdf).  
+  GitHub: [asdf](https://github.com/asdf)
+  
+  ## License
+
+This project is licensed under the Mozilla license. For more information, please visit [this link](https://opensource.org/licenses/MPL-2.0).
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 import fs from "fs";
 import inquirer from "inquirer";
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -33,7 +34,7 @@ const questions = [
     type: "input",
     name: "test",
     message:
-      "Please write a brief explain of how users can test the application/project",
+      "Please write a brief explanation of how users can test the application/project",
   },
   {
     type: "list",
@@ -52,7 +53,6 @@ const questions = [
     type: "input",
     name: "userName",
     message: "What is your GitHub username?",
-    validate: validateInput,
   },
   {
     type: "input",
@@ -61,47 +61,24 @@ const questions = [
   },
 ];
 
-// Function for markdown license badges
-
-function getLicense(value) {
-  if (value === "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  } else if (value === "Apache 2.0") {
-    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-  } else if (value === "Mozilla") {
-    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-  } else if (value === "GNU GPLv3") {
-    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-  } else if (value === "GNU AGPLv3") {
-    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
-  } else if (value === "GNU LGPLv3") {
-    return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
-  }
-}
-
-function validateInput(value) {
-    if (value != "") {
-        return true;
-    } else {
-        return "Please answer the question with some kind on input.";
-    }
-}
-
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, generateMarkdown(data), function (err) {
+  fs.writeFile(fileName, data, function (err) {
     if (err) {
       return console.log(err);
     }
+    console.log('README.md has been generated!');
   });
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
-    inquirer.prompt(questions).then((responses) => {
-      writeToFile("README.md", responses);
-    });
-  }
+  inquirer.prompt(questions).then((data) => {
+    console.log(JSON.stringify(data, null, " "));
+    const readmeContent = generateMarkdown(data);
+    writeToFile("README.md", readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
